29f01d9fa370c6d5baa299e895b62bb4f776ffc7
agirb <agirbasomer35@gmail.com>
2022-04-21 16:44:59 +0300

    SHAPE

---
+++ src/day39_Recap/shapeTask/Rectangle.java
@@ -0,0 +1,29 @@
+package day39_Recap.shapeTask;
+
+public class Rectangle {
+    private double length, width;
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setLength(double length) {
+        if(length<=0){
+            System.err.println("ınvalid length: "+length);
+            System.exit(1);
+        }
+        this.length = length;
+    }
+
+    public double getWidth() {
+        return width;
+    }
+
+    public void setWidth(double width) {
+        if(width<=0){
+            System.err.println("ınvalid width: "+width);
+            System.exit(1);
+        }
+        this.width = width;
+    }
+}

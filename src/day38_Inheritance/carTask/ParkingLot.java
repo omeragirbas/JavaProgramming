4fafeb4f9f02eef289fa4647b382167e2de9190d
agirb <agirbasomer35@gmail.com>
2022-04-21 11:58:57 +0300

    car 2

---
+++ src/day38_Inheritance/carTask/ParkingLot.java
@@ -0,0 +1,10 @@
+package day38_Inheritance.carTask;
+
+public class ParkingLot {
+    public static void main(String[] args) {
+        Toyota toyota=new Toyota("Carmy ", 2020,20000,"GRAY",123456);
+        Tesla tesla=new Tesla("model s",2022,94990,"red",0);
+        Bmw bmw=new Bmw("x5",1990,78890,"grey",233);
+
+    }
+}

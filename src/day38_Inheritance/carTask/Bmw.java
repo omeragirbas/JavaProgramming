6b9206f04d5894b2b377f0cfb556b0ac8f5c9d7e
agirb <agirbasomer35@gmail.com>
2022-04-21 11:22:41 +0300

    car 2

--- src/day38_Inheritance/carTask/Bmw.java
+++ src/day38_Inheritance/carTask/Bmw.java
@@ -1,7 +1,7 @@
 package day38_Inheritance.carTask;
 
-public class BMW extends Car{
-    public BMW( String model, int year, double price, String color, int miles) {
+public class Bmw extends Car{
+    public Bmw( String model, int year, double price, String color, int miles) {
         super("BMW", model, year, price, color, miles);
     }
     public void breakDown(){
